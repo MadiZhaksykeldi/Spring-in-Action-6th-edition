acee6e11f5b5c4ef440d37f9282112075aea0177
Madi Zhaksykeldi <madizhaksykeldi@Madis-MacBook-Air.local>
2023-02-20 12:54:48 +0600

    Added Spring MVC

--- src/test/java/com/example/tacocloud/HomeControllerTest.java
+++ src/test/java/com/example/tacocloud/HomeControllerTest.java
@@ -10,6 +10,7 @@ import static
 import static
         org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
+import tacos.web.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
